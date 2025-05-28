c43513a0460c9240b51b4f33432173c840ac18a8
Shouvik1Sarkar <shouvik3501@gmail.com>
2025-05-28 23:11:02 +0530

    some array method questions

--- JS-Foundation-SpeedRun/02_iteration_functions_concepts/README.md
+++
@@ -1,2 +0,0 @@
-call snd bind
-closure
---
+++ JS-Live-Class/object-array-class/8thFeb.js
@@ -0,0 +1,45 @@
+// This is Just a revision for last class (JS conditionals and Loops)
+
+// Problem:Create an arra containing different types of teas
+
+const teas = [
+  "Green Tea",
+  "Black Tea",
+  "Oolong Tea",
+  "White Tea",
+  "Herbal Tea",
+];
+
+// Problem: Add "Chamomile Tea" to the existing of teas
+teas.push("Chamomile Tea");
+// Problem: Remove "Oolong tea"
+if (teas.indexOf("Oolong Tea") > -1) {
+  teas.splice(teas.indexOf("Oolong Tea"), 1);
+}
+// Problem: filter the list with teas which are caffinated
+const caffeinatedTea = teas.filter((tea) => tea !== "Herbal Tea");
+// console.log(caffeinatedTea);
+
+// Problem: sort in alphabetical order
+
+teas.sort();
+
+const test = ["🥚", "🐓"];
+test.sort();
+
+// Problem: Use for loop to pring each array
+
+for (let i = 0; i < teas.length; i++) {
+  //   console.log(teas[i]);
+}
+
+// Problem: count the number of caffeinated teas
+let count = 0;
+for (let i = 0; i < teas.length; i++) {
+  if (teas[i] !== "Herbal Tea") {
+    count += 1;
+  }
+}
+console.log(count); // 4 bcz oolong teas was removed and one was added
+// Problem:
+// Problem:
