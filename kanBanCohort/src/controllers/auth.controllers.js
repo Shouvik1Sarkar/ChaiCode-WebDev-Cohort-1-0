7ce8fe76c42d7ce470a37fbb2217fd93b631b1d0
Shouvik1Sarkar <shouvik3501@gmail.com>
2025-08-18 17:08:35 +0530

    delete env

--- kanBanCohort/src/controllers/auth.controllers.js
+++ kanBanCohort/src/controllers/auth.controllers.js
@@ -1,11 +1,27 @@
 import { asyncHandler } from "../utils/async-handler.js";
 import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/api-error.js";
 const registerUser = asyncHandler(async (req, res) => {
   const { email, username, password, role } = req.body;
 
   //validation
 
   const existedUser = await User.findOne({ email });
+  if (existedUser) {
+    throw new ApiError(500, "User already exists.");
+  }
+  const existedUserName = await User.findOne({ username });
+
+  if (existedUserName) {
+    throw new ApiError(500, "User Name already exists.");
+  }
+
+  const user = await User.create({
+    username,
+    email,
+    password,
+    role,
+  });
 });
 
 const loginUser = asyncHandler(async (req, res) => {
