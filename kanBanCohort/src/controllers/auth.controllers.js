71c12470cff2c8b7ceb1143f858a2a7fb5c03277
Shouvik1Sarkar <shouvik3501@gmail.com>
2025-08-18 16:38:02 +0530

    start megaproject (kanban) assignments

---
+++ kanBanCohort/src/controllers/auth.controllers.js
@@ -0,0 +1,75 @@
+import { asyncHandler } from "../utils/async-handler.js";
+import { User } from "../models/user.models.js";
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+
+  const existedUser = await User.findOne({ email });
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const verifyEmail = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const resendEmailVerification = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+const resetForgottenPassword = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const forgotPasswordRequest = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+
+  //validation
+});
+
+export {
+  changeCurrentPassword,
+  forgotPasswordRequest,
+  getCurrentUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  resendEmailVerification,
+  resetForgottenPassword,
+  verifyEmail,
+};
